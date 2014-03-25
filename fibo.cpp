e415bc95bd67ece17428e1e6e0d807bf991387c2
Gerardo Bravo Gómez <wesitos_56@hotmail.com>
2014-03-25 17:31:40 -0600

    Update fibo.cpp

--- fibo.cpp
+++ fibo.cpp
@@ -3,9 +3,9 @@ int main(){
 	printf("SERIE DE FIBONACCI \n \n INGRESE UN NUMERO:  ");
 	int a;
 	scanf("%d", &a);
-	int v1=0, v2=1, r=0;
+	int v1=0, v2=1, r=0, i;
 	
-	for (int i =0; i<a; i++){
+	for (i=0; i<a; i++){
 		printf(" %d \n", r);
 		r= v1+v2;
 		v1=v2;
