f3ab5ad9b06ac88acab64c90d4371e093b11370c
Gerardo Bravo Gómez <wesitos_56@hotmail.com>
2014-03-25 18:23:06 -0600

    Ejercicios en c

--- fig_3.cpp
+++ fig_3.cpp
@@ -2,22 +2,23 @@
 
 int main()
 {
-	printf("INGRESE UN NUMERO:  ");
-	int a;
+	printf("INGRESE UN NUMERO: ");
+	int a,j,i,t;
 	scanf("%d", &a);
-	printf(" \n FIGURA 3:  \n");
-	int j=a;
-	for (int i=0;i<=j;i++)
+	printf(" \n FIGURA 3: \n");
+	j=a;
+	t=a;
+	for (i=0;i<=t;i++)
 	{
-		for(int j=a; j>0; j--){
+		for(j=a; j>0; j--){
 			printf(" ");
-		}
-		for(int j=0 ; j<i;j++){
-			printf("*");	
-		}
-			
-		printf("\n");
-		a--;
+			}
+		for(j=0 ; j<i;j++){
+			printf("*");
+			}
+	printf("\n");
+	a--;
 	}
-		return 0;
-}  
+	return 0;
+} 
+
